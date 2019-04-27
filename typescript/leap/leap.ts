6e7c85d2deffb41f3be4261d314d2dcd9210b157
haydenbetts <haydenbetts@gmail.com>
2019-04-27 16:37:59 -0700

    leap

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,13 @@
+function isLeapYear( year: number ) {
+    if (year % 4 === 0) {
+      if (year % 100 === 0 && !(year % 400 === 0)) return false
+      return true;
+    }
+    return false;
+}
+
+// on every year that is evenly divisible by 4
+  // except every year that is evenly divisible by 100
+  // unless the year is also evenly divisible by 400
+
+export default isLeapYear
